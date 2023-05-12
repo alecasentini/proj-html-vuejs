140ee22ebf7ed5d4d760c04a5be79e0856004977
alecasentini <ale.casentini@gmail.com>
2023-05-12 16:31:01 +0200

    Integrazione Fontawesome

--- src/main.js
+++ src/main.js
@@ -4,4 +4,12 @@ import App from './App.vue'
 
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"
 
-createApp(App).mount('#app')
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+library.add(faStar);
+
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+createApp(App)
+    .mount('#app')
+    .component("font-awesome-icon", FontAwesomeIcon)
