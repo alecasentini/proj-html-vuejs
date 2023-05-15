31c0f0c46c747743e6eaeac3c5f69aa4c7f06f04
alecasentini <ale.casentini@gmail.com>
2023-05-15 12:26:45 +0200

    Completato Header

--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -12,10 +12,49 @@ export default {
 </script>
 
 <template>
-    <NavbarComp />
+    <div class="w-90">
+        <NavbarComp />
+    </div>
     <hr>
-    <div class="d-flex">
+    <div class="d-flex w-90 py-3 justify-content-between">
         <img src="../../public/MasterStudy_logo.svg" alt="logo">
+
+        <div class="d-flex align-items-center">
+            <div class="d-flex align-items-center">
+                <font-awesome-icon :icon="['fas', 'bars']" />
+                <span class="ms-1">CATEGORY</span>
+            </div>
+            <div class="input-group ms-2">
+                <input type="text" class="form-control py-3" placeholder="Search courses" aria-describedby="basic-addon1">
+                <span class="input-group-text text-white px-4"><font-awesome-icon
+                        :icon="['fas', 'magnifying-glass']" /></span>
+            </div>
+        </div>
+
+        <div class="d-flex align-items-center">
+            <div class="d-flex align-items-center">
+                <font-awesome-icon :icon="['fas', 'bullhorn']" style="color: #195ec8;" />
+                <span class="ms-1"> <a href="#">Become an Instructor</a> </span>
+            </div>
+            <div class="d-flex align-items-center">
+                <font-awesome-icon :icon="['fas', 'briefcase']" class="ms-4" style="color: #195ec8;" />
+                <span class="ms-1"> <a href="#">For Enterprise</a> </span>
+            </div>
+        </div>
+
+        <div class="d-flex align-items-center">
+            <div class="d-flex align-items-center">
+                <font-awesome-icon :icon="['far', 'user']" style="color: #195ec8;" />
+                <span class="ms-1" style="color: #195ec8;">Log in</span>
+            </div>
+
+            <button type="button" class="btn ms-4">SIGN UP</button>
+
+            <font-awesome-icon :icon="['far', 'bookmark']" class="ms-4" style="color: #195ec8; font-size: 22px;" />
+        </div>
+
+
+
 
     </div>
 </template>
@@ -24,5 +63,40 @@ export default {
 hr {
     color: #77777a;
     margin: 0;
+}
+
+.w-90 {
+    width: 90%;
+    margin: 0 auto;
+}
+
+.input-group {
+    width: 500px;
+
+    .form-control {
+        background-color: #f0f2f5;
+        color: #77777a;
+        border-radius: 0;
+        border: 0;
+    }
+
+    .input-group-text {
+        border-radius: 0;
+        border: 0;
+        background-color: #195ec8;
+    }
+}
+
+a {
+    color: #77777a;
+    text-decoration: none;
+}
+
+.btn {
+    background-color: #195ec8;
+    color: white;
+    font-weight: bolder;
+    border-radius: 25px;
+    padding: 10px 30px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -5,9 +5,10 @@ import App from './App.vue'
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"
 
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faBasketball } from '@fortawesome/free-solid-svg-icons'
+import { faBasketball, faBars, faMagnifyingGlass, faBullhorn, faBriefcase } from '@fortawesome/free-solid-svg-icons'
 import { faTwitter, faInstagram, faBehance, faFlickr, faGit, faLinkedin, faPinterest } from '@fortawesome/free-brands-svg-icons'
-library.add(faBasketball, faTwitter, faInstagram, faBehance, faFlickr, faGit, faLinkedin, faPinterest);
+import { faUser, faBookmark } from "@fortawesome/free-regular-svg-icons"
+library.add(faBasketball, faBars, faMagnifyingGlass, faBullhorn, faBriefcase, faUser, faBookmark, faTwitter, faInstagram, faBehance, faFlickr, faGit, faLinkedin, faPinterest);
 
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
